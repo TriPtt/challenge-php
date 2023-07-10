68e099992500db0c1869d1b3becac8cef97e29fa
Tristan Petit <tristan.petit01@gmail.com>
2023-07-10 10:08:35 +0200

    exo1

---
+++ primitive-declarations.php
@@ -0,0 +1,9 @@
+<?php
+
+$firstStr = "a string of words";
+$firstEscapeStr = "a string of words \n";
+$firstBool = true;
+$firstInteger = 5;
+$firstFloat = 1.5;
+$firstNull = null;
+$firstArray = [2, null, 'Hi', 5.5, false];
